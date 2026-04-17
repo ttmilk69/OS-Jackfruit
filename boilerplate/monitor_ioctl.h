2f1c07faaba80b0a97aab5fe653b6a3a629d1f66
Jyeshta J <jyeshtaj2006@gmail.com>
2026-04-17 12:18:55 +0530

    Refactor monitor_ioctl.h for cleaner structure
    Refactor monitor_request structure and update ioctl definitions.

--- boilerplate/monitor_ioctl.h
+++ boilerplate/monitor_ioctl.h
@@ -1,25 +1,12 @@
 #ifndef MONITOR_IOCTL_H
 #define MONITOR_IOCTL_H
-
-#ifdef __KERNEL__
 #include <linux/ioctl.h>
-#include <linux/types.h>
-#else
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#endif
-
-#define MONITOR_NAME_LEN 32
-
+#define MONITOR_REGISTER _IOW('a','a', struct monitor_request) #define
+MONITOR_UNREGISTER _IOW('a','b', struct monitor_request)
 struct monitor_request {
-    pid_t pid;
-    unsigned long soft_limit_bytes;
-    unsigned long hard_limit_bytes;
-    char container_id[MONITOR_NAME_LEN];
+int pid;
+unsigned long soft_limit_bytes;
+unsigned long hard_limit_bytes; char
+container_id[32];
 };
-
-#define MONITOR_MAGIC 'M'
-#define MONITOR_REGISTER _IOW(MONITOR_MAGIC, 1, struct monitor_request)
-#define MONITOR_UNREGISTER _IOW(MONITOR_MAGIC, 2, struct monitor_request)
-
 #endif
